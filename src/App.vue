13752b14d1cab4d82d5245d05fd6d1c419e04ff0
Éamonn Reilly <eamonn.reilly@liberty-it.co.uk>
2020-06-10 15:43:59 +0100

    make todo app

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
-  </v-app>
+  <todo-list list-name="My to-dos" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
+import TodoList from "./components/TodoList.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+    TodoList
+  }
 };
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
